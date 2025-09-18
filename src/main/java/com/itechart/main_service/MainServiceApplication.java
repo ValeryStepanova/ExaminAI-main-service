7f0bbfceb494f9d05948c3de7160ff91a85cc4ac
Olmosbek510 <olmosbekorazboyev@gmail.com>
2025-09-18 15:44:17 +0500

    docker env config implemented

--- src/main/java/com/itechart/main_service/MainServiceApplication.java
+++ src/main/java/com/itechart/main_service/MainServiceApplication.java
@@ -1,12 +1,11 @@
 package com.itechart.main_service;
 
 import com.itechart.main_service.config.GitHubConfig;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-@SpringBootApplication//exclude = OpenAiAudioSpeechAutoConfiguration.class)
+@SpringBootApplication
 @EnableFeignClients(basePackages = "com.itechart.admin_service_api.api")
 @EnableConfigurationProperties(GitHubConfig.class)
 public class MainServiceApplication {
